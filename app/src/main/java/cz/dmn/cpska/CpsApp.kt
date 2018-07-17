a97405a57b40604c8c8d52da4550ba41b4eb789c
Tomas Kletzander <tomas.kletzander@gmail.com>
2018-07-17 21:13:06 +0200

    Added Fabric initialization

--- app/src/main/java/cz/dmn/cpska/CpsApp.kt
+++ app/src/main/java/cz/dmn/cpska/CpsApp.kt
@@ -1,10 +1,14 @@
 package cz.dmn.cpska
 
 import android.app.Application
+import com.crashlytics.android.Crashlytics
+import com.crashlytics.android.answers.Answers
+import io.fabric.sdk.android.Fabric
 
 class CpsApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        Fabric.with(this, Crashlytics(), Answers())
     }
 }
